7ec18d72f0f20c0186087e41c499acc2e0b415bb
Sultonov557 <sultonovzerifboy@gmail.com>
2023-11-09 21:30:56 +0500

    .

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,8 +1,9 @@
 import * as fs from "fs";
 import { Question } from "./entity/question.entity";
 import { join } from "path";
+import { User } from "./entity/user.entity";
 
-const users = fs.readFileSync(join(__dirname, "../../src/database/users.json"));
+const users: User[] = JSON.parse(fs.readFileSync(join(__dirname, "../../src/database/users.json")).toLocaleString());
 const questions: Question[] = JSON.parse(fs.readFileSync(join(__dirname, "../../src/database/questions.json")).toLocaleString());
 
 export function newQuestion(question: Question) {
